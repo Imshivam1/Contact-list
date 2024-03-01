9fedb8812fff82ae586280967cbabc4e57c718e2
Shivam Kumar Manjhi <113127627+Imshivam1@users.noreply.github.com>
2024-03-02 00:20:13 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require('express'); // Importing Express.js
 const path = require('path'); // Importing Path module
 const port = 8000; // Declaring the server port
 
+const db = require('./config/mongoose');
+const Contact = require('./models/contact');
+
 // Initializing Express app
 const app = express();
 
@@ -13,7 +16,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 // Using middleware to parse form data and serve static assets
-app.use(express.urlencoded()); // Middleware to parse form data
+app.use(express.urlencoded({ extended: true })); // Middleware to parse form data
 app.use(express.static('assets')); // Serving static assets from the 'assets' directory
 
 // Middleware to log request received time
@@ -22,21 +25,23 @@ app.use(function(req, res, next){
     next();
 });
 
-// Creating a contact list array
-var contactList = [
-    { name: "John Doe", email:"john@doe.com" },
-    { name: "Jane Smith", email:"jane@smith.com" },
-    { name: "Shiva", email:"shivar@shiv.com"}
-];
-
 // Handling GET request for the root URL '/'
 app.get('/', function(req, res) {
-    // Rendering 'home.ejs' view and passing data to the template
-    return res.render('home',{ 
-        title : 'My Contact List', 
-        contact_list : contactList
-    });
+    // Find all contacts from the database
+    Contact.find({})
+        .then(contacts => {
+            // Rendering 'home.ejs' view and passing data to the template
+            return res.render('home',{ 
+                title : 'My Contact List', 
+                contact_list : contacts
+            });
+        })
+        .catch(err => {
+            console.log('Error in fetching contacts from db:', err);
+            return res.status(500).send('Internal Server Error');
+        });
 });
+
 
 // Handling GET request for '/practice' route
 app.get('/practice', function(req, res){
@@ -55,31 +60,47 @@ app.get('/myroute', (req, res) => {
     res.render('mytemplate', { title, fruits });
 });
 
-// For deleting a contact 
-app.get('/delete-contact', function(req, res){
-    
-    let email = req.query.email;//get the query from the url
-
-    let contactIndex = contactList.findIndex(contact => contact.email == email);
-
-    if (contactIndex != -1){
-        contactList.splice(contactIndex, 1);
-    }
-
-    return res.redirect('back');
-
+// Handling GET requests for '/create-contact' route
+app.get('/create-contact', function(req, res) {
+    // Rendering the 'home.ejs' view to display the form
+    res.render('home', { 
+        title: 'Create New Contact', // Setting the title of the page
+        contact_list: [] // No need to pass contact list data here since it will be fetched from the database
+    });
 });
 
 // Handling POST request for '/create-contact' route
-app.post('/create-contact', function(req, res){
-    // Adding new contact to contactList array from form data
-    contactList.push({
-        name : req.body.name,
-        email : req.body.email
-    });
+app.post('/create-contact', async function(req, res){
+    try {
+        // Check if name and email are provided
+        if (!req.body.name || !req.body.email) {
+            throw new Error('Name and email are required.');
+        }
+        
+        // Creating new contact
+        const newContact = await Contact.create({
+            name: req.body.name,
+            email: req.body.email
+        });
 
-    // Redirecting back to the root URL '/'
-    return res.redirect('/');
+        console.log('*****', newContact);
+        // Redirect back to home page after creating new contact
+        res.redirect('/');
+    } catch (err) {
+        console.error('Error in saving new contact: ', err);
+        res.status(500).send('Error in saving new contact: ' + err.message);
+    }
+});
+
+// Handling 404 errors
+app.use(function(req, res, next) {
+    res.status(404).send("Sorry can't find that!");
+});
+
+// Handling other errors
+app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
 });
 
 // Starting the server on specified port
